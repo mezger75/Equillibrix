ac16ea127d3606dbcc47885079d4803b65b279d8
mezger75 <9771804@gmail.com>
2024-11-19 22:24:35 +0300

    fix: connect button height

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,7 +1,9 @@
 export const Header = () => {
     return (
         <header className="flex w-full p-4 justify-end">
-            <appkit-account-button />
+            <div className="h-8">
+                <appkit-account-button />
+            </div>
         </header>
     );
 };
