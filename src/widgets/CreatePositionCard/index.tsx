70165dd2d497fcc4aa314a6af0cd086ebb9d14ae
mezger75 <9771804@gmail.com>
2024-11-22 22:18:28 +0300

    fix: wallet connection with appKit modal

--- src/widgets/CreatePositionCard/index.tsx
+++ src/widgets/CreatePositionCard/index.tsx
@@ -1,11 +1,11 @@
 import { Button } from '@nextui-org/react';
 import { InputAmount, SliderAmount, TitleAmount } from './_components';
 import { useAccount } from 'wagmi';
-import { useWalletConnect } from '@/shared/hooks/useWalletConnect';
+import { useAppKit } from '@reown/appkit/react';
 
 export const CreatePositionCard = () => {
     const { isConnected, chain } = useAccount();
-    const { handleConnectWallet } = useWalletConnect();
+    const { open } = useAppKit();
 
     return (
         <div className="flex flex-col gap-6 p-4 md:mt-4 w-full bg-[rgb(31,33,45)] bg-opacity-100 rounded-lg md:w-1/2 md:max-w-none max-w-md">
@@ -18,7 +18,7 @@ export const CreatePositionCard = () => {
             {isConnected ? (
                 <Button>Submit Deposit</Button>
             ) : (
-                <Button onClick={handleConnectWallet}>Connect Wallet</Button>
+                <Button onClick={() => open()}>Connect Wallet</Button>
             )}
         </div>
     );
