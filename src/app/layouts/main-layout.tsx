5cbddd8365fe1809e9d1f0d47d2b6541346d6804
mezger75 <9771804@gmail.com>
2024-11-24 21:31:41 +0300

    feat: add toast and pending/loading status for better UX

--- src/app/layouts/main-layout.tsx
+++ src/app/layouts/main-layout.tsx
@@ -1,10 +1,13 @@
+import { toasterOptions } from '@/shared/config/toaster-options';
 import { Header } from '@/widgets/header';
+import { Toaster } from 'react-hot-toast';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex flex-col h-screen container mx-auto">
             <Header />
             {children}
+            <Toaster position='bottom-right' toastOptions={toasterOptions} />
         </div>
     );
 };
--- src/widgets/CreatePositionCard/index.tsx
+++ src/widgets/CreatePositionCard/index.tsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react';
 import { Button } from '@nextui-org/react';
 import { InputAmount, SliderAmount, TitleAmount } from './_components';
-import { useAccount, useWaitForTransactionReceipt, useWriteContract, type BaseError } from 'wagmi';
+import { useAccount, useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
 import { useAppKit } from '@reown/appkit/react';
 import {
     CONTRACT_ABI_LONG_POSITION,
@@ -8,13 +9,15 @@ import {
 } from '@/shared/config/contracts';
 import { useStore } from '@/shared/hooks/useStore';
 import { parseUnits } from 'viem';
+import toast from 'react-hot-toast';
 
 export const CreatePositionCard = () => {
     const { isConnected, chain } = useAccount();
     const { open } = useAppKit();
 
-    const { data: hash, writeContract, isPending, error } = useWriteContract();
+    const { data: hash, writeContract, isPending, error: writeError } = useWriteContract();
     const { amount, leverage } = useStore();
+
     const convertedSupplyAmount = parseUnits(amount.toString(), 18);
     const convertedLeverage = leverage * 1000;
     const convertedSlippage = 500;
@@ -28,21 +31,35 @@ export const CreatePositionCard = () => {
             value: convertedSupplyAmount,
         });
     };
-
-    const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
+    // TODO check isLoading with real RPC
+    const {
+        error: receiptError,
+        status,
+        isLoading,
+    } = useWaitForTransactionReceipt({
         hash,
     });
 
-    console.log(
-        'Hash',
-        hash,
-        'ERROR',
-        error,
-        'isConfirming',
-        isConfirming,
-        'isConfirmed',
-        isConfirmed,
-    );
+    useEffect(() => {
+        if (writeError) {
+            toast.dismiss();
+            toast.error(writeError.message);
+        }
+
+        if (status === 'success') {
+            toast.dismiss();
+            toast.success('Successfully created position');
+        }
+
+        if (status === 'error') {
+            toast.dismiss();
+            toast.error(`Transaction failed: ${receiptError?.name}`);
+        }
+
+        return () => {
+            toast.dismiss();
+        };
+    }, [status, receiptError, writeError]);
 
     return (
         <div className="flex flex-col gap-6 p-4 md:mt-4 w-full bg-[rgb(31,33,45)] bg-opacity-100 rounded-lg md:w-1/2 md:max-w-none max-w-md">
@@ -51,10 +68,14 @@ export const CreatePositionCard = () => {
             <InputAmount />
 
             <SliderAmount />
-            {isConfirmed && <div className="text-green-500">Success</div>}
+
             {isConnected ? (
-                <Button onClick={handleSubmit} disabled={isPending}>
-                    {isPending ? 'Submitting...' : 'Submit Deposit'}
+                <Button onClick={handleSubmit} isLoading={isPending || isLoading}>
+                    {isPending
+                        ? 'Please confirm in your wallet...'
+                        : isLoading
+                          ? 'Submitting...'
+                          : 'Submit Deposit'}
                 </Button>
             ) : (
                 <Button onClick={() => open()}>Connect Wallet</Button>
