444f7baf8e5b2409815a5f6cae1ab5873b27a182
HUAWEICOMP\Vladi <vow001@yandex.ru>
2024-12-04 14:52:48 +0300

    #8-fixed background

--- src/app/layouts/main-layout.tsx
+++ src/app/layouts/main-layout.tsx
@@ -2,7 +2,7 @@ import { Header } from '@/widgets/header';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="flex flex-col h-screen container mx-auto">
+        <div className="flex flex-col min-h-screen container mx-auto">
             <Header />
             {children}
         </div>
