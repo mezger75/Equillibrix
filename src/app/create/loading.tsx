2c3bbf4f79f16a5a59040032510d55371946d7c0
mezger75 <9771804@gmail.com>
2025-01-12 16:56:08 +0300

    fix loading page styles

--- src/app/create/loading.tsx
+++ src/app/create/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@nextui-org/react';
 
 export default function Loading() {
     return (
-        <div>
+        <div className="flex max-h-screen flex-grow items-center justify-center gap-2">
             <Spinner size="lg" />
             <span>Loading...</span>
         </div>
