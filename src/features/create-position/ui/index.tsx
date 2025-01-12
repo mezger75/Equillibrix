e63152243970ad9ced5c5a507e1b939b96dbaa45
mezger75 <9771804@gmail.com>
2025-01-12 16:55:42 +0300

    fix page refreshing on button click

--- src/features/create-position/ui/index.tsx
+++ src/features/create-position/ui/index.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { Link } from '@nextui-org/react';
 import { CreatePositionCard } from '@/widgets/CreatePositionCard';
 import { BackButton } from '@/shared/ui';
+import { useRouter } from 'next/navigation';
 
 export const CreatePosition = () => {
+    const router = useRouter();
     return (
         <div className="flex flex-col items-center w-full overflow-auto gap-4 pb-6">
             <div className="self-start">
-                <BackButton href="/" as={Link} />
+                <BackButton onClick={() => router.push('/')} className={`bg-opacity-0`} />
             </div>
 
             <CreatePositionCard />
--- src/shared/ui/BackButton/index.tsx
+++ src/shared/ui/BackButton/index.tsx
@@ -1,9 +1,14 @@
 import { Button, ButtonProps } from '@nextui-org/react';
 import { ArrowBackIcon } from './_components/ArrowBackIcon';
 
-export const BackButton = ({ as, href, className }: React.PropsWithChildren<ButtonProps>) => {
+export const BackButton = ({
+    as,
+    href,
+    className,
+    ...props
+}: React.PropsWithChildren<ButtonProps>) => {
     return (
-        <Button className={className} as={as} href={href} style={{ background: 'none' }}>
+        <Button className={className} as={as} href={href} {...props}>
             <ArrowBackIcon />
             <span className="text-base">Back</span>
         </Button>
